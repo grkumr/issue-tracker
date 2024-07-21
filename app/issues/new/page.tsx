450b9cfbd9f00c29578ae39aca4802bd8becf584
grkumr <grmk2023@gmail.com>
2024-07-21 09:43:31 +0530

    Handle form submit

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -18,16 +18,21 @@ const NewIssuePage = () => {
     const router = useRouter();
     const {register, control, handleSubmit} = useForm<IssueForm>();
   return (
-    <form className="max-w-xl space-y-5"  onSubmit={handleSubmit(async (data) => {
-        await axios.post('/api/issues', data);
-        router.push('/issues');
+    <form className="max-w-xl space-y-5"
+      onSubmit={handleSubmit(async (data) => {
+        try {
+          await axios.post('/api/issues', data);
+          router.push('/issues');          
+        } catch (error) {
+          console.log(error)
+        }
     })}>
-        <TextField.Root placeholder='Title' {...register('title')} >            
+        <TextField.Root placeholder='Title' {...register('title')} >              
         </TextField.Root>
         <Controller 
           name="description"
           control={control}
-          render={({ field }) =>  <SimpleMDE placeholder='Description' />}
+          render={({ field }) =>  <SimpleMDE placeholder='Description' {...field} />}
         />
        
         <Button>Submit New Issue</Button>
